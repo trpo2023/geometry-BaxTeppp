765f6224556bc8b91f897d87dab230cfa3fc3e1b
Попков Алексей Павлович <iv221s23@awp05.csc.local>
2023-02-22 09:30:23 +0700

    added scanf for structure

--- geom.c
+++ geom.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #define pi 3.14
-typedef struct point{
+int main(){
+typedef struct Point{
     float x;
     float y;
 } point;
+typedef struct circle{
+    char name[100];
+    point krug;
+    float radius;
+} circle;
+printf("Введите фигуру координаты и радиус\n");
+circle circle1;
+scanf("%f,%f,%f",&circle1.krug.x,&circle1.krug.y,&circle1.radius);
+printf("%f %f %f",circle1.krug.x ,circle1.krug.y,circle1.radius);
+return 0;
+}
+
+
+
+
+
 
